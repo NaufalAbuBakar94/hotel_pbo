dc1768d8e760784645f351b9a6863170c8797854
Naufal Abu Bakar <54340211+NaufalAbuBakar94@users.noreply.github.com>
2022-03-28 21:03:05 +0700

    Add files via upload

---
+++ hotel.py
@@ -0,0 +1,471 @@
+def garis():
+    print ("=====================================")
+
+print("================================================= HOTEL SMK JAKARTA PUSAT 1 =================================================")
+print(" -- lama inap -- SUPERIOR -- DULUXE -- PREMIUM --")
+print(" -- 1/2hari -- 200.000/hari -- 150.000/hari -- 100.000/hari")
+
+tipe_kamar = input("Masukkan tipe kamar : ")
+lama_inap = int(input("Masukkan lama menginap : "))
+
+if tipe_kamar == "superior":
+    if lama_inap <= 2 :
+        total_harga = 200000*lama_inap
+    elif lama_inap <= 4 :
+        total_harga = 200000*lama_inap
+    else:
+        total_harga = 600000*lama_inap
+
+elif tipe_kamar == "deluxe":
+    if lama_inap <= 2 :
+        total_harga = 150000*lama_inap
+    elif lama_inap <= 4 :
+        total_harga = 150000*lama_inap
+    else:
+        total_harga = 150000*lama_inap
+
+elif tipe_kamar == "premium":
+    if lama_inap <= 2 :
+        total_harga = 100000*lama_inap
+    elif lama_inap <= 4 :
+        total_harga = 100000*lama_inap
+    else:
+        total_harga = 100000*lama_inap
+
+
+garis()
+print("Kamar yang dipilih : ",(tipe_kamar))
+print("Lama inap : ",(lama_inap))
+print("Total Harga : ",(total_harga))
+
+garis()
+
+tombol = input("Kembali ke menu utama ? (Y/N) ")
+garis()
+
+if tombol == "Y":
+    tipe_kamar = input("Masukkan tipe kamar : ")
+    lama_inap = int(input("Masukkan lama menginap : "))
+
+else:
+    print("Terima Kasih telah menggunakan jasa layanan Hotel Kami")
+
+if tipe_kamar == "superior":
+    if lama_inap <= 2 :
+        total_harga = 200000*lama_inap
+    elif lama_inap <= 4 :
+        total_harga = 200000*lama_inap
+    else:
+        total_harga = 600000*lama_inap
+
+elif tipe_kamar == "deluxe":
+    if lama_inap <= 2 :
+        total_harga = 150000*lama_inap
+    elif lama_inap <= 4 :
+        total_harga = 150000*lama_inap
+    else:
+        total_harga = 150000*lama_inap
+
+elif tipe_kamar == "premium":
+    if lama_inap <= 2 :
+        total_harga = 100000*lama_inap
+    elif lama_inap <= 4 :
+        total_harga = 100000*lama_inap
+    else:
+        total_harga = 100000*lama_inap
+
+
+garis()
+print("Kamar yang dipilih : ",(tipe_kamar))
+print("Lama inap : ",(lama_inap))
+print("Total Harga : ",(total_harga))
+
+garis()
+
+tombol = input("Kembali ke menu utama ? (Y/N) ")
+garis()
+
+if tombol == "Y":
+    tipe_kamar = input("Masukkan tipe kamar : ")
+    lama_inap = int(input("Masukkan lama menginap : "))
+
+else:
+    print("Terima Kasih telah menggunakan jasa layanan Hotel Kami")
+
+if tipe_kamar == "superior":
+    if lama_inap <= 2 :
+        total_harga = 200000*lama_inap
+    elif lama_inap <= 4 :
+        total_harga = 200000*lama_inap
+    else:
+        total_harga = 600000*lama_inap
+
+elif tipe_kamar == "deluxe":
+    if lama_inap <= 2 :
+        total_harga = 150000*lama_inap
+    elif lama_inap <= 4 :
+        total_harga = 150000*lama_inap
+    else:
+        total_harga = 150000*lama_inap
+
+elif tipe_kamar == "premium":
+    if lama_inap <= 2 :
+        total_harga = 100000*lama_inap
+    elif lama_inap <= 4 :
+        total_harga = 100000*lama_inap
+    else:
+        total_harga = 100000*lama_inap
+
+
+garis()
+print("Kamar yang dipilih : ",(tipe_kamar))
+print("Lama inap : ",(lama_inap))
+print("Total Harga : ",(total_harga))
+
+garis()
+
+tombol = input("Kembali ke menu utama ? (Y/N) ")
+garis()
+
+if tombol == "Y":
+    tipe_kamar = input("Masukkan tipe kamar : ")
+    lama_inap = int(input("Masukkan lama menginap : "))
+
+else:
+    print("Terima Kasih telah menggunakan jasa layanan Hotel Kami")
+
+if tipe_kamar == "superior":
+    if lama_inap <= 2 :
+        total_harga = 200000*lama_inap
+    elif lama_inap <= 4 :
+        total_harga = 200000*lama_inap
+    else:
+        total_harga = 600000*lama_inap
+
+elif tipe_kamar == "deluxe":
+    if lama_inap <= 2 :
+        total_harga = 150000*lama_inap
+    elif lama_inap <= 4 :
+        total_harga = 150000*lama_inap
+    else:
+        total_harga = 150000*lama_inap
+
+elif tipe_kamar == "premium":
+    if lama_inap <= 2 :
+        total_harga = 100000*lama_inap
+    elif lama_inap <= 4 :
+        total_harga = 100000*lama_inap
+    else:
+        total_harga = 100000*lama_inap
+
+
+garis()
+print("Kamar yang dipilih : ",(tipe_kamar))
+print("Lama inap : ",(lama_inap))
+print("Total Harga : ",(total_harga))
+
+garis()
+
+tombol = input("Kembali ke menu utama ? (Y/N) ")
+garis()
+
+if tombol == "Y":
+    tipe_kamar = input("Masukkan tipe kamar : ")
+    lama_inap = int(input("Masukkan lama menginap : "))
+
+else:
+    print("Terima Kasih telah menggunakan jasa layanan Hotel Kami")
+
+if tipe_kamar == "superior":
+    if lama_inap <= 2 :
+        total_harga = 200000*lama_inap
+    elif lama_inap <= 4 :
+        total_harga = 200000*lama_inap
+    else:
+        total_harga = 600000*lama_inap
+
+elif tipe_kamar == "deluxe":
+    if lama_inap <= 2 :
+        total_harga = 150000*lama_inap
+    elif lama_inap <= 4 :
+        total_harga = 150000*lama_inap
+    else:
+        total_harga = 150000*lama_inap
+
+elif tipe_kamar == "premium":
+    if lama_inap <= 2 :
+        total_harga = 100000*lama_inap
+    elif lama_inap <= 4 :
+        total_harga = 100000*lama_inap
+    else:
+        total_harga = 100000*lama_inap
+
+
+garis()
+print("Kamar yang dipilih : ",(tipe_kamar))
+print("Lama inap : ",(lama_inap))
+print("Total Harga : ",(total_harga))
+
+garis()
+
+tombol = input("Kembali ke menu utama ? (Y/N) ")
+garis()
+
+if tombol == "Y":
+    tipe_kamar = input("Masukkan tipe kamar : ")
+    lama_inap = int(input("Masukkan lama menginap : "))
+
+else:
+    print("Terima Kasih telah menggunakan jasa layanan Hotel Kami")
+
+if tipe_kamar == "superior":
+    if lama_inap <= 2 :
+        total_harga = 200000*lama_inap
+    elif lama_inap <= 4 :
+        total_harga = 200000*lama_inap
+    else:
+        total_harga = 600000*lama_inap
+
+elif tipe_kamar == "deluxe":
+    if lama_inap <= 2 :
+        total_harga = 150000*lama_inap
+    elif lama_inap <= 4 :
+        total_harga = 150000*lama_inap
+    else:
+        total_harga = 150000*lama_inap
+
+elif tipe_kamar == "premium":
+    if lama_inap <= 2 :
+        total_harga = 100000*lama_inap
+    elif lama_inap <= 4 :
+        total_harga = 100000*lama_inap
+    else:
+        total_harga = 100000*lama_inap
+
+
+garis()
+print("Kamar yang dipilih : ",(tipe_kamar))
+print("Lama inap : ",(lama_inap))
+print("Total Harga : ",(total_harga))
+
+garis()
+
+tombol = input("Kembali ke menu utama ? (Y/N) ")
+garis()
+
+if tombol == "Y":
+    tipe_kamar = input("Masukkan tipe kamar : ")
+    lama_inap = int(input("Masukkan lama menginap : "))
+
+else:
+    print("Terima Kasih telah menggunakan jasa layanan Hotel Kami")
+
+if tipe_kamar == "superior":
+    if lama_inap <= 2 :
+        total_harga = 200000*lama_inap
+    elif lama_inap <= 4 :
+        total_harga = 200000*lama_inap
+    else:
+        total_harga = 600000*lama_inap
+
+elif tipe_kamar == "deluxe":
+    if lama_inap <= 2 :
+        total_harga = 150000*lama_inap
+    elif lama_inap <= 4 :
+        total_harga = 150000*lama_inap
+    else:
+        total_harga = 150000*lama_inap
+
+elif tipe_kamar == "premium":
+    if lama_inap <= 2 :
+        total_harga = 100000*lama_inap
+    elif lama_inap <= 4 :
+        total_harga = 100000*lama_inap
+    else:
+        total_harga = 100000*lama_inap
+
+
+garis()
+print("Kamar yang dipilih : ",(tipe_kamar))
+print("Lama inap : ",(lama_inap))
+print("Total Harga : ",(total_harga))
+
+garis()
+
+tombol = input("Kembali ke menu utama ? (Y/N) ")
+garis()
+
+if tombol == "Y":
+    tipe_kamar = input("Masukkan tipe kamar : ")
+    lama_inap = int(input("Masukkan lama menginap : "))
+
+else:
+    print("Terima Kasih telah menggunakan jasa layanan Hotel Kami")
+
+if tipe_kamar == "superior":
+    if lama_inap <= 2 :
+        total_harga = 200000*lama_inap
+    elif lama_inap <= 4 :
+        total_harga = 200000*lama_inap
+    else:
+        total_harga = 600000*lama_inap
+
+elif tipe_kamar == "deluxe":
+    if lama_inap <= 2 :
+        total_harga = 150000*lama_inap
+    elif lama_inap <= 4 :
+        total_harga = 150000*lama_inap
+    else:
+        total_harga = 150000*lama_inap
+
+elif tipe_kamar == "premium":
+    if lama_inap <= 2 :
+        total_harga = 100000*lama_inap
+    elif lama_inap <= 4 :
+        total_harga = 100000*lama_inap
+    else:
+        total_harga = 100000*lama_inap
+
+
+garis()
+print("Kamar yang dipilih : ",(tipe_kamar))
+print("Lama inap : ",(lama_inap))
+print("Total Harga : ",(total_harga))
+
+garis()
+
+tombol = input("Kembali ke menu utama ? (Y/N) ")
+garis()
+
+if tombol == "Y":
+    tipe_kamar = input("Masukkan tipe kamar : ")
+    lama_inap = int(input("Masukkan lama menginap : "))
+
+else:
+    print("Terima Kasih telah menggunakan jasa layanan Hotel Kami")
+
+if tipe_kamar == "superior":
+    if lama_inap <= 2 :
+        total_harga = 200000*lama_inap
+    elif lama_inap <= 4 :
+        total_harga = 200000*lama_inap
+    else:
+        total_harga = 600000*lama_inap
+
+elif tipe_kamar == "deluxe":
+    if lama_inap <= 2 :
+        total_harga = 150000*lama_inap
+    elif lama_inap <= 4 :
+        total_harga = 150000*lama_inap
+    else:
+        total_harga = 150000*lama_inap
+
+elif tipe_kamar == "premium":
+    if lama_inap <= 2 :
+        total_harga = 100000*lama_inap
+    elif lama_inap <= 4 :
+        total_harga = 100000*lama_inap
+    else:
+        total_harga = 100000*lama_inap
+
+
+garis()
+print("Kamar yang dipilih : ",(tipe_kamar))
+print("Lama inap : ",(lama_inap))
+print("Total Harga : ",(total_harga))
+
+garis()
+
+tombol = input("Kembali ke menu utama ? (Y/N) ")
+garis()
+
+if tombol == "Y":
+    tipe_kamar = input("Masukkan tipe kamar : ")
+    lama_inap = int(input("Masukkan lama menginap : "))
+
+else:
+    print("Terima Kasih telah menggunakan jasa layanan Hotel Kami")
+
+if tipe_kamar == "superior":
+    if lama_inap <= 2 :
+        total_harga = 200000*lama_inap
+    elif lama_inap <= 4 :
+        total_harga = 200000*lama_inap
+    else:
+        total_harga = 600000*lama_inap
+
+elif tipe_kamar == "deluxe":
+    if lama_inap <= 2 :
+        total_harga = 150000*lama_inap
+    elif lama_inap <= 4 :
+        total_harga = 150000*lama_inap
+    else:
+        total_harga = 150000*lama_inap
+
+elif tipe_kamar == "premium":
+    if lama_inap <= 2 :
+        total_harga = 100000*lama_inap
+    elif lama_inap <= 4 :
+        total_harga = 100000*lama_inap
+    else:
+        total_harga = 100000*lama_inap
+
+
+garis()
+print("Kamar yang dipilih : ",(tipe_kamar))
+print("Lama inap : ",(lama_inap))
+print("Total Harga : ",(total_harga))
+
+garis()
+
+tombol = input("Kembali ke menu utama ? (Y/N) ")
+garis()
+
+if tombol == "Y":
+    tipe_kamar = input("Masukkan tipe kamar : ")
+    lama_inap = int(input("Masukkan lama menginap : "))
+
+else:
+    print("Terima Kasih telah menggunakan jasa layanan Hotel Kami")
+
+if tipe_kamar == "superior":
+    if lama_inap <= 2 :
+        total_harga = 200000*lama_inap
+    elif lama_inap <= 4 :
+        total_harga = 200000*lama_inap
+    else:
+        total_harga = 600000*lama_inap
+
+elif tipe_kamar == "deluxe":
+    if lama_inap <= 2 :
+        total_harga = 150000*lama_inap
+    elif lama_inap <= 4 :
+        total_harga = 150000*lama_inap
+    else:
+        total_harga = 150000*lama_inap
+
+elif tipe_kamar == "premium":
+    if lama_inap <= 2 : 
+        total_harga = 100000*lama_inap
+    elif lama_inap <= 4 :
+        total_harga = 100000*lama_inap
+    else:
+        total_harga = 100000*lama_inap
+
+
+garis()
+print("Kamar yang dipilih : ",(tipe_kamar))
+print("Lama inap : ",(lama_inap))
+print("Total Harga : ",(total_harga))
+
+garis()
+
+tombol = input("Kembali ke menu utama ? (Y/N) ")
+garis()
+
+if tombol == "Y":
+    tipe_kamar = input("Masukkan tipe kamar : ")
+    lama_inap = int(input("Masukkan lama menginap : "))
+
+else:
+    print("Terima Kasih telah menggunakan jasa layanan Hotel Kami")
